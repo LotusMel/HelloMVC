862bdc2afd471ff5ead3ecdfe83efb48c70097e7
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-05-19 18:32:59 -0500

    Hello/Goodbye

--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -9,17 +9,7 @@ namespace HelloMVC.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/Hello/Display'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet me!' />" +
-                "</form>";
-
-            return Content(html, "text/html");
-        }
-
-        public IActionResult Display(string name = "World")
-        {
-            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+            return Content("<h1>Hello World</h1>", "text/html");
         }
         
         public IActionResult Goodbye()
