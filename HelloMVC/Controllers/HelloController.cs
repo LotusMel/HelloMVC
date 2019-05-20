af9cf3e8053b9ef8b6d662f5716be9ef590e92e5
LotusMel <46916651+LotusMel@users.noreply.github.com>
2019-05-19 19:15:40 -0500

    Routes-using a form to display greeting

--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -7,14 +7,55 @@ namespace HelloMVC.Controllers
     public class HelloController : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        /*
+        public IActionResult Index(string name)//Enables index to accept query
         {
-            return Content("<h1>Hello World</h1>", "text/html");
+            //Default for no name
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "World";
+            }
+
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+        */
+
+        //Or...
+
+        public IActionResult Index()//Enables index to accept query
+        {
+            string html = "<form method='post' action='/Hello/Display'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        //***Post request*** Form
+
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
         }
         
+
+        // /Hello/Goodbye
+        // alter the route to this controller to be: /Hello/Aloha
+        /*
+        // Using 'Route Atribute'
+        [Route("/Hello/Aloha")]
         public IActionResult Goodbye()
         {
             return Content("Goodbye");
         }
+        */
+        // Change to display in various ways
+        // Respond to query string. Pass string name to Index
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+
     }
 }
